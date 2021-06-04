f6491c78d8c34a61571c682815886d327a280a72
unknown <vedant9901@gmail.com>
2021-06-05 00:45:02 +0530

    Updates

---
+++ precache-manifest.2d4479e98ae6d37399b419761fa7c264.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f356b7d433c87322d6f68f7df1688b0",
+    "url": "/Portfoilio/index.html"
+  },
+  {
+    "revision": "2449aedc29ffde422907",
+    "url": "/Portfoilio/static/css/2.49ecd109.chunk.css"
+  },
+  {
+    "revision": "0d8814b9d29f005cbd3e",
+    "url": "/Portfoilio/static/css/main.fc723d98.chunk.css"
+  },
+  {
+    "revision": "2449aedc29ffde422907",
+    "url": "/Portfoilio/static/js/2.eeca512b.chunk.js"
+  },
+  {
+    "revision": "ca51b41385d08e2222a6a1a3623fc89c",
+    "url": "/Portfoilio/static/js/2.eeca512b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d8814b9d29f005cbd3e",
+    "url": "/Portfoilio/static/js/main.706fb4b4.chunk.js"
+  },
+  {
+    "revision": "fb1fef8af339b6380e96",
+    "url": "/Portfoilio/static/js/runtime-main.b53a0914.js"
+  }
+]);
